69252d4d78a81464edf3a13ecb3cd13dfc8bd351
kiharaaaa <dodgeball.tigers.1@gmail.com>
2022-08-10 15:11:30 +0900

    no message

---
+++ EggRun/Assets/Script/PlayerMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float CheckPointDistance; // CheckPoint間の距離
+    public float speed; // 進むスピード
+    double level; // いくつCheckPointを通過したか
+    public static int lane; // 今どのレーンにいるか、左：0 中央：1 右：2
+    public GameObject MainCamera;
+
+    void Start()
+    {
+        level = 1;
+        lane = 1;
+    }
+
+    void Update()
+    {
+        if((int)this.transform.position.z > CheckPointDistance * level)
+        {
+            level++;
+            speed *= 1.1f;
+        }
+        this.transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+
+        Debug.Log(lane);
+    }
+
+    public void MoveLeft()
+    {
+        if (lane != 0)
+        {
+            this.transform.position += new Vector3(-2, 0, 0);
+            MainCamera.transform.position += new Vector3(2, 0, 0);
+            lane --;
+        }
+    }
+
+    public void MoveRight()
+    {
+        if (lane != 2)
+        {
+            this.transform.position += new Vector3(2, 0, 0);
+            MainCamera.transform.position += new Vector3(-2, 0, 0);
+            lane ++;
+        }
+    }
+}
