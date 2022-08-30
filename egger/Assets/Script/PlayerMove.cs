7fa6ba6b424f21e63dfcfe632c18e1a6c516dbe6
kiharaaaa <dodgeball.tigers.1@gmail.com>
2022-08-31 00:10:03 +0900

    完成

---
+++ egger/Assets/Script/PlayerMove.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    float CheckPointDistance = 30.0f; // CheckPoint間の距離
+    public static float speed; // 進むスピード
+    public static float deltaSpeed;
+    public static int level; // いくつCheckPointを通過したか
+    public static int lane; // 今どのレーンにいるか、左：0 中央：1 右：2
+    public GameObject MainCamera;
+    public GameObject paperFubuki;
+    [SerializeField] private ParticleSystem particle1;
+    [SerializeField] private ParticleSystem particle2;
+    [SerializeField] private ParticleSystem particle3;
+
+    public static PlayerMove playerMove;
+
+    void Start()
+    {
+        level = 1;
+        lane = 1;
+        speed = 3;
+        deltaSpeed = 1.0f;
+        playerMove = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // スクリーンショットを保存
+            ScreenCapture.CaptureScreenshot("ScreenShot.png");
+        }
+
+        if ((int)this.transform.position.z > CheckPointDistance * level)
+        {
+            level++;
+            speed += deltaSpeed;
+            if(deltaSpeed > 0.1f) deltaSpeed -= 0.05f;
+        }
+        this.transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+
+        if (GameSystem.GameOverFlag == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetMouseButtonDown(0))
+            {
+                MoveLeft();
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetMouseButtonDown(1))
+            {
+                MoveRight();
+            }
+        }
+    }
+
+    public void MoveLeft()
+    {
+        if (lane != 0)
+        {
+            this.transform.position += new Vector3(-2, 0, 0);
+            MainCamera.transform.position += new Vector3(2, 0, 0);
+            lane --;
+        }
+    }
+
+    public void MoveRight()
+    {
+        if (lane != 2)
+        {
+            this.transform.position += new Vector3(2, 0, 0);
+            MainCamera.transform.position += new Vector3(-2, 0, 0);
+            lane ++;
+        }
+    }
+
+    public void PlayFubuki()
+    {
+        particle1.Play();
+        particle2.Play();
+        particle3.Play();
+    }
+}
---
+++ egger/Assets/Script/RuleSystem.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RuleSystem : MonoBehaviour
+{
+    int slidePoint;
+    int animePoint;
+    int n;
+    public GameObject RuleBeginImage;
+    public GameObject[] Rule1Image;
+    public GameObject[] Rule2Image;
+    public GameObject[] RLImage;
+    public GameObject RuleEndImage;
+    public AudioSource ButtonSE;
+
+    int flag0 = 0, flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0;
+
+    public void OnClick()
+    {
+        ButtonSE.Play(0);
+    }
+
+    void Start()
+    {
+        slidePoint = 0;
+        animePoint = 0;
+        n = 2;
+        False();
+    }
+
+    void Update()
+    {
+        if (slidePoint == -1) // タイトルへ
+        {
+            SceneManager.LoadScene("1.Title");
+        }
+        else if(slidePoint == 0) // 開始
+        {
+            if (flag0 == 0)
+            {
+                RuleBeginImage.gameObject.SetActive(true);
+                flag0 = 1;
+                flag1 = 0;
+            }
+        }
+        else if (slidePoint == 1) // ルール概要1
+        {
+            n = 2;
+            if (flag1 == 0)
+            {
+                ShowNextSlide();
+                flag0 = 0;
+                flag1 = 1;
+                flag2 = 0;
+            }
+        }
+        else if (slidePoint == 2) // ルール概要2
+        {
+            n = 2;
+            if (flag2 == 0)
+            {
+                ShowNextSlide();
+                flag1 = 0;
+                flag2 = 1;
+                flag3 = 0;
+            }
+        }
+        else if (slidePoint == 3) // 左右
+        {
+            n = 8;
+            if (flag3 == 0)
+            {
+                ShowNextSlide();
+                flag2 = 0;
+                flag3 = 1;
+                flag4 = 0;
+            }
+        }
+        else if (slidePoint == 4) // 終了
+        {
+            if (flag4 == 0)
+            {
+                RuleEndImage.gameObject.SetActive(true);
+                flag3 = 0;
+                flag4 = 1;
+            }
+        }
+        else if (slidePoint == 5) // プレイ画面へ
+        {
+            SceneManager.LoadScene("3.Play");
+        }
+    }
+
+    void False() // 全ての画像を非表示
+    {
+        RuleBeginImage.gameObject.SetActive(false);
+        for (int i = 0; i < 2; i++) Rule1Image[i].gameObject.SetActive(false);
+        for (int i = 0; i < 2; i++) Rule2Image[i].gameObject.SetActive(false);
+        for (int i = 0; i < 8; i++) RLImage[i].gameObject.SetActive(false);
+        RuleEndImage.gameObject.SetActive(false);
+    }
+
+    void ShowNextSlide()
+    {
+        False();
+        if (slidePoint == 1)
+        {
+            Rule1Image[animePoint].gameObject.SetActive(true);
+        }
+        else if (slidePoint == 2)
+        {
+            Rule2Image[animePoint].gameObject.SetActive(true);
+        }
+        else if(slidePoint == 3)
+        {
+            RLImage[animePoint].gameObject.SetActive(true);
+        }
+        
+        animePoint++;
+        animePoint %= n;
+        Invoke("ShowNextSlide", 0.7f);
+    }
+
+    public void BackButton()
+    {
+        slidePoint--;
+        animePoint = 0;
+        CancelInvoke();
+    }
+    public void NextButton()
+    {
+        slidePoint++;
+        animePoint = 0;
+        CancelInvoke();
+    }
+
+}
